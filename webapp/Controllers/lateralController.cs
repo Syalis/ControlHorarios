24e8c8d75643727848a6e51831a81240c2ecaca6
pedro.moreno <pedro.moreno@topdigital.es>
2019-08-20 09:27:35 +0200

    Crt: clase lateral y sesion de usuario para fichajes

---
+++ webapp/Controllers/lateralController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using webapp.Models;
+
+namespace webapp.Controllers
+{
+    public class lateralController : Controller
+    {
+        //Metodo para iniciar el dropdown con id y nombres
+        [HttpPost]
+        public JsonResult getNombresDropdown()
+        {
+            RespGeneric resp = new RespGeneric("KO");
+            resp.msg = string.Empty;
+
+            try
+            {
+                //Comprobacion del estado del boton+fichajes del mes en curso+totales de horas
+               
+                resp.d.Add("data", Data.lateral.getNombresDropdown());
+                resp.cod = "OK";
+                resp.msg = "Exito en la petición";
+            }
+            catch (Exception e)
+            {
+                resp.msg = e.Message;
+            }
+            return Json(resp);
+
+        }
+
+
+    }
+}
---
+++ webapp/Data/lateral.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using webapp.Helpers;
+
+namespace webapp.Data
+{
+    public class lateral
+    {
+        //Metodo para obtener todos los usuarios y su id
+        public static List<Dictionary<string, object>> getNombresDropdown()
+        {
+            return BD.getQueryResult($"SELECT id, upper(concat_ws(' ', nombre, primer_apellido, segundo_apellido)) as nombre FROM usuarios");
+        }
+    }
+}
