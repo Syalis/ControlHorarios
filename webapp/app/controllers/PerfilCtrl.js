e623201f53ef115d591886156a1a3319e32cef90
pedro.moreno <pedro.moreno@topdigital.es>
2019-08-27 09:13:29 +0200

    UPD: cambios para permisos usuario

---
+++ webapp/app/controllers/PerfilCtrl.js
@@ -0,0 +1,8 @@
+﻿angular.module('webapp').controller('PerfilCtrl', ["$scope", "$http", "$window", PerfilCtrl]);
+
+function PerfilCtrl($scope, $http, $window) {
+    var vm = this;
+    vm.session = $window.sessionStorage;
+
+
+}
